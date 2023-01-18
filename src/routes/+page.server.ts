1d13b4b674394a211711bf39d714f193da5394d2
Ivan Krishanik <krishanik.ivan@gmail.com>
2023-01-18 18:26:52 +0100

    add sample

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,12 @@
+/** @type {import('./$types').Actions} */
+export const actions = {
+  foo: async (event) => {
+    console.log("foo:")
+    //console.log(event)
+  },
+  bar: async (event) => {
+    console.log("bar:")
+    //console.log(event)
+  }
+};
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import Fa from 'svelte-fa'
+import { faFlag } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+</script>
+
+<Fa icon={faFlag} />
+<Fa icon={faGithub} />
+
+<form method="POST" action="?/foo">
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname" value="John"><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname" value="Doe"><br><br>
+    <input type="submit" value="Submit">
+  </form> 
+
+  <form method="POST" action="?/bar" style="margin-top:100px;">
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname" value="sdfsdfsdf"><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname" value="Dosdfsdfsdfe"><br><br>
+    <input type="submit" value="Submit">
+  </form> 
